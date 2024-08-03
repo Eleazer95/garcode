0e2efa4eb5037b49f637af81a2886a9d9d85a389
user <Pastor Gar>
2024-08-03 08:49:56 +0100

    Initial commit

--- garcode_project/settings/base.py
+++ garcode_project/settings/base.py
@@ -143,7 +143,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # Add this line
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
